2dc1fe84c5fa78818379806271152381a8ebf25f
Mihail Bobeica <mihailbobeica.07@gmail.com>
2023-04-10 09:23:34 -0400

    update display blocks

--- analysis/beautifier.py
+++ analysis/beautifier.py
@@ -2,8 +2,11 @@
 
 
 from binascii import hexlify, unhexlify
+from math import ceil
 
 from colorama import Fore, Back
+
+from exception_analysis import ColsNotInRange
 
 
 def _warning(message):
@@ -11,10 +14,11 @@ def _warning(message):
 
 
 def _print_property(name, value):
-	print(f"{Fore.CYAN}{name}{Fore.RESET}\t{value}")
+	print(f"{Fore.GREEN}{name}{Fore.RESET}\t{value}")
 
 
-def display_blocks(stream, block_size=16):
+def display_blocks(stream, block_size=16, cols=2):
+	print()
 	print(f"{Back.MAGENTA}DISPLAY BLOCKS{Back.RESET}")
 	if isinstance(stream, str):
 		# check if it's an hexadecimal string
@@ -37,16 +41,49 @@ def display_blocks(stream, block_size=16):
 
 	if t:=(length % block_size):
 		_warning(f"Stream length is not divisible by the block size\n{length} % {block_size} = {t}")
-	blocks = length // block_size
-	_print_property("blocks", blocks)
+	n = ceil(length / block_size)
+	_print_property("blocks", n)
+
+	blocks = [stream[i * hex_block_size: (i+1) * hex_block_size] for i in range(n)]
+
+	match cols:
+		case 1:
+			for i, block in enumerate(blocks):
+				if i % 2:
+					print(f"{Fore.CYAN}{block}{Fore.RESET}", end="\t")
+				else:
+					print(block, end="\t")
+				print()
+		case 2:
+			for i, block in enumerate(blocks):
+				if ((i + 1) // 2) % 2:
+					print(f"{Fore.CYAN}{block}{Fore.RESET}", end="\t")
+				else:
+					print(block, end="\t")
+				if (i + 1) % 2 == 0:
+					print()
+		case 3:
+			for i, block in enumerate(blocks):
+				if i % 2:
+					print(f"{Fore.CYAN}{block}{Fore.RESET}", end="\t")
+				else:
+					print(block, end="\t")
+				if (i + 1) % 3 == 0:
+					print()
+		case _:
+			raise ColsNotInRange()
+
+	print()
 
 
 
 def main():
-	a = "-" + "ab" * 16 + "bc" * 15
-	display_blocks(a)
+	a = "-" + "ab" * 16 + "bc" * 15 + "ab" * 16 + "bc" * 15 + "ab" * 16 + "bc" * 15 + "12" * 64
+	display_blocks(a, cols=1)
 	b = "ab" * 16 + "bc" * 16
 	display_blocks(b)
+	c = "00" * 64
+	display_blocks(c)
 
 
 if __name__ == '__main__':
---
+++ analysis/exception_analysis.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+class ColsNotInRange(Exception):
+	pass
+
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+	main()
