25756c847d5dfb9d5818241d4fc5c0a789fd4f16
Mihail Bobeica <mihailbobeica.07@gmail.com>
2023-04-10 07:27:25 -0400

    restore stashed files

--- analysis/beautifier.py
+++ analysis/beautifier.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 
 
+from binascii import hexlify, unhexlify
+
 from colorama import Fore, Back
 
 
+def _warning(message):
+	print(f"{Fore.YELLOW}WARNING: {message}{Fore.RESET}")
+
+
 def display_blocks(stream, block_size=16):
-	pass
+	if isinstance(stream, str):
+		# check if it's an hexadecimal string
+		stream = stream.strip()
+		int(stream, 16)  # short way to check if the stream is hexadecimal
+		if stream[0] == "-":
+			_warning("There is a negative sign in front of the hex stream.")
+		stream = stream.replace("0x", "").replace("0X", "").replace("-", "")
+	elif isinstance(stream, bytes):
+		pass
+	elif isinstance(stream, bytearray):
+		pass
+	else:
+		raise TypeError
 
 
 def main():
